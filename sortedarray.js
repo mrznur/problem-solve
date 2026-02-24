2f2735abb908ae82d659064da2213d4982618760
mrznur <mahmudunmiraz@gmail.com>
2026-02-24 22:16:16 +0600

    solved leetcode 24

---
+++ sortedarray.js
@@ -0,0 +1,13 @@
+var mergeKLists = function (lists) {
+  if (!lists || lists.length === 0) {
+    return null;
+    
+  }
+};
+
+x = [
+  [1, 4, 5],
+  [1, 3, 4],
+  [2, 6],
+];
+console.log(mergeKLists(x));
---
+++ swapNodes.js
@@ -0,0 +1,22 @@
+var swapPairs = function(head) {
+    let dummy = new ListNode(0);
+    dummy.next = head;
+    
+    let prev = dummy;
+    
+    while (prev.next !== null && prev.next.next !== null) {
+        let first = prev.next;
+        let second = prev.next.next;
+
+        first.next = second.next;
+        second.next = first;
+        prev.next = second;
+
+        prev = first;
+    }
+    
+    return dummy.next;
+};
+
+a = [1,2,3,4];
+console.log(swapPairs(a));
